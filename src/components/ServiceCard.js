45b32d7000135ef5590bf4a95a7b8794ea740d88
Daniel Yeo <y_daniel@naver.com>
2025-08-20 23:08:18 +0900

    remove animation

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -12,9 +12,8 @@ export default function ServiceCard({ title, description, icon: Icon }) {
       {/* 배경 장식용 라이트 그라데이션 */}
       <div
         className="
-         absolute inset-0 opacity-0 group-hover:opacity-100
-         bg-gradient-to-br from-brandPurple/20 via-brandGreen/15 to-transparent
-         transition-opacity duration-300
+         opacity-0 group-hover:opacity-100
+         bg-gradient-to-br duration-300
        "
       />
       <div className="p-6 flex flex-col gap-4">
